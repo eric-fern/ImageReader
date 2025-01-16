23fd8006186ff6c5f720001a238a6fe6248b4063
Eric Fernandez <eric.fernandez5@outlook.com>
2025-01-16 18:24:17 +0000

    Added appsettings and ignored my app settings from gitignore. Keep APIKEy safe in appsettings json

--- SampleApp/ImageService/Program.cs
+++ SampleApp/ImageService/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+//Azure AI imports
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+/*
+Present user with an image upload button.
+User uploads image of ingredients label
+Azure AI form recognizer provides an analysis about what you're eating and how healthy it is.
+*/
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string endpoint = "https://imagereaderinstance.cognitiveservices.azure.com/";
+        string apiKey = "";
+        string imagePath = "<path-to-your-image>";
+
+        var credential = new AzureKeyCredential(apiKey);
+        var client = new DocumentAnalysisClient(new Uri(endpoint), credential);
+
+        using var stream = new FileStream(imagePath, FileMode.Open);
+
+        var operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", stream);
+        var result = operation.Value;
+
+        foreach (var page in result.Pages)
+        {
+            foreach (var line in page.Lines)
+            {
+                Console.WriteLine(line.Content);
+            }
+        }
+    }
+}
